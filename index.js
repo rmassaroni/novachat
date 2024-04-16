75c44e4de75646b7d11c613956b5f4260f192759
rjm <robjmass@gmail.com>
2024-04-16 16:13:06 -0400

    index.html

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const { Server } = require("socket.io");
 
 const express = require('express');
 const { createServer } = require('node:http');
+const { join } = require('node:path')
 
 const app = express();
 const server = createServer(app);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    //res.send('<h1>Hello world</h1>');
+    res.sendFile(join(__dirname, 'index.html'));
 });
 
 server.listen(3000, () => {
