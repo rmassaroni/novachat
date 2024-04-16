5a25bd7d1652f13be2663644a036cc4b640644e1
rjm <robjmass@gmail.com>
2024-04-16 16:39:23 -0400

    chat msg fix

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg)
-        io.emit('new mesage: ', msg);
+        io.emit('chat message', msg);
     });
     socket.on('disconnect', () => {
         console.log('user disconnected');
