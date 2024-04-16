91a1cbd5113a201cc73fb64aae1e1f4ad7b2575e
rjm <robjmass@gmail.com>
2024-04-16 16:17:36 -0400

    socket.io setup

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const { Server } = require("socket.io");
-
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path')
+const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
     //res.send('<h1>Hello world</h1>');
     res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 server.listen(3000, () => {
