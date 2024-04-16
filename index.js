44417e81c7946f785f61eb0dbc4f1b30fefc6a55
rjm <robjmass@gmail.com>
2024-04-16 16:21:56 -0400

    user disconnected

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 server.listen(3000, () => {
