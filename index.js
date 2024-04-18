6e39aa9ab4601c80b13245a6638c91bd1b0341fe
rjm <robjmass@gmail.com>
2024-04-18 16:35:37 -0400

    internet info capabilities

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.join('new room');
-    console.log(socket.rooms);
+    //console.log(socket.rooms);
     //socket.emit(socket.rooms);
     //io.emit('hello');
     socket.on('global message', (msg) => {
@@ -36,8 +36,30 @@ io.on('connection', (socket) => {
     socket.emit('chat message', 'chat message')
     socket.emit('global message', 'global message');
 
+    socket.emit('global message', 'welcome');
+    socket.emit('global message', 'Connected to: ' + wifiName);
+
 });
+
+//wifi info
+let wifiName = "";
+const { exec } = require('child_process');
+const command = 'powershell.exe -Command "Get-NetConnectionProfile | Select-Object -ExpandProperty Name"';
+
 
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`Wi-Fi name: ${stdout}`);
+        wifiName = stdout.trim();
+        //socket.emit('global message', wifiName);
+    });
 });
