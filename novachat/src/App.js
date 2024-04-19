e5371b80ea4d2154273a2663ac6e382298d99360
rjm <robjmass@gmail.com>
2024-04-19 14:41:11 -0400

    front end structure

--- novachat/src/App.js
+++ novachat/src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const containerRef = useRef(null);
+    const handleKeyPress = (e) => {
+        const container = containerRef.current;
+        if (!container) return;
+
+        const pageWidth = container.clientWidth;
+        const scrollAmount = pageWidth;
+        if (e.key === 'j') {
+            container.scrollLeft -= scrollAmount;
+        } else if (e.key === 'k') {
+            container.scrollLeft += scrollAmount;
+        }
+    };
+    return (
+
+        <div>
+            <div className="App-header">
+                <header className="App-header">
+                    {/* <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload. test
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                */}
+                    <h1>novachat</h1>
+                    <div className="line">
+                        <p1>Server Messages: </p1>
+                        <div class="divider"></div>
+                        <input type="text" className="input-class" />
+                    </div>
+                </header>
+
+            </div>
+            <div className="container" ref={containerRef} tabIndex="0" onKeyDown={handleKeyPress}>
+
+                <div className="page">Page 1</div>
+                <div className="page">Page 2</div>
+                <div className="page">Page 3</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
