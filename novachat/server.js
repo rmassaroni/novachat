2e48857628fa68f045d2c62ef34a0b99856fe855
rjm <robjmass@gmail.com>
2024-04-20 16:16:48 -0400

    server.js

---
+++ novachat/server.js
@@ -0,0 +1,26 @@
+//following imports are from original index.js. still figuring out where they should be
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path')
+const { Server } = require('socket.io');
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+//socket stuff from original index.js
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello world</h1>');
+    res.sendFile(join(__dirname, 'index.html'));
+});
+io.on('connection', (socket) => {
+    console.log("user connected");
+    socket.on('message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('message', msg);
+    });
+    socket.emit('message', 'test');
+});
+server.listen(3000, () => {
+    console.log('server running at http://localhost:3000');
+});
+
--- novachat/src/App.js
+++ novachat/src/App.js
@@ -2,32 +2,6 @@ import logo from './logo.svg';
 import React, { useRef } from 'react';
 import './App.css';
 
-//following imports are from original index.js. still figuring out where they should be
-const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path')
-const { Server } = require('socket.io');
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
-
-//socket stuff from original index.js
-let channels = [];
-app.get('/', (req, res) => {
-    //res.send('<h1>Hello world</h1>');
-    res.sendFile(join(__dirname, 'index.html'));
-});
-io.on('connection', (socket) => {
-    console.log("user connected");
-    socket.on('message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('message', msg);
-    });
-    socket.emit('message', 'test');
-});
-
-
-
 function App() {
     const containerRef = useRef(null);
     const handleKeyPress = (e) => {
