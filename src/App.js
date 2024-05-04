5c80c6550a68f492d8018fc2ee368fdcc5cde6b9
rjm <robjmass@gmail.com>
2024-05-04 18:45:11 -0400

    AppHeader.js component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
+import AppHeader from "./AppHeader";
 import Sidebar from "./Sidebar";
 import RefreshButton from "./RefreshButton";
 import Page from "./Page";
@@ -210,58 +211,7 @@ function App() {
 
     return (
         <div style={{ backgroundColor: "#6d6dd4" }}>
-            <div
-                className="App-header"
-                style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    paddingRight: "10px",
-                    height: "70px" }}>
-                <div class="line" style={{ alignItems: "center" }}>
-                    <input
-                        style={{ width: "50px", height: "50px", padding: "10px", paddingRight: "15px" }}
-                        type="image"
-                        alt="sidebar icon"
-                        src="https://cdn-icons-png.flaticon.com/128/10486/10486773.png"
-                        onClick={() => setSidebar(!sidebar)}/>
-                    <div class="line">
-                        <h2>Channel: Global</h2>
-                    </div>
-                </div>
-                <div style={{ position: "relative", display: "inlineBlock"}}>
-                    <img style={{ marginTop: "38px" }}src="/banner.png" alt="novachat banner" />
-                    <h1 style={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        padding: "1px",
-                        fontSize: "3.6em",
-                        marginTop: "6px"
-                    }}>novachat</h1>
-                </div>
-                <div class="line" style={{justifyContent: "flex-end", alignItems: "center" }}>
-                    <div class="line" style={{ alignItems: "center" }}>
-                        {editingUsername ? (
-                            <>
-                                <input type="text" value={editedUsername} onChange={(e) => setEditedUsername(e.target.value)} />
-                                <button onClick={handleUsernameSave}>Save</button>
-                            </>
-                        ) : (
-                                <h2 style={{ paddingRight: "10px", marginLeft: "-40px" }}>Chatting as: {username}</h2>
-                            )}
-                        <input 
-                            style={{ width: "20px", height: "20px" }}
-                            type="image"
-                            alt="edit icon"
-                            src="/edit-icon.png"
-                            onClick={handleUsernameEdit} />
-                    </div>
-                    <img style={{ width: "50px", height: "50px", padding: "0px", borderRadius: "10px", border: "1px solid #ccc" }} src="/nova.jpg" alt="nova icon" />
-                </div>
-            </div>
+            <AppHeader sidebar={sidebar} setSidebar={setSidebar} editingUsername={editingUsername} editedUsername={editedUsername} setEditedUsername={setEditedUsername} handleUsernameSave={handleUsernameSave} username={username} handleUsernameEdit={handleUsernameEdit} />
             <div style={{ display: "flex", flexDirection: "row" }}>
                 <Sidebar myChannels={myChannels} roomUpdate={roomUpdate} sidebar={sidebar} setSideBar={setSidebar} />
                 <div className="container" ref={containerRef} tabIndex="0" onKeyDown={handleKeyPress}>
---
+++ src/AppHeader.js
@@ -0,0 +1,59 @@
+
+const AppHeader = ({ sidebar, setSidebar, editingUsername, editedUsername, setEditedUsername, handleUsernameSave, username, handleUsernameEdit }) => {
+    return (
+        <div
+            className="App-header"
+            style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingRight: "10px",
+                height: "70px" }}>
+            <div class="line" style={{ alignItems: "center" }}>
+                <input
+                    style={{ width: "50px", height: "50px", padding: "10px", paddingRight: "15px" }}
+                    type="image"
+                    alt="sidebar icon"
+                    src="https://cdn-icons-png.flaticon.com/128/10486/10486773.png"
+                    onClick={() => setSidebar(!sidebar)}/>
+                <div class="line">
+                    <h2>Channel: Global</h2>
+                </div>
+            </div>
+            <div style={{ position: "relative", display: "inlineBlock"}}>
+                <img style={{ marginTop: "38px" }}src="/banner.png" alt="novachat banner" />
+                <h1 style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    padding: "1px",
+                    fontSize: "3.6em",
+                    marginTop: "6px"
+                }}>novachat</h1>
+            </div>
+            <div class="line" style={{justifyContent: "flex-end", alignItems: "center" }}>
+                <div class="line" style={{ alignItems: "center" }}>
+                    {editingUsername ? (
+                        <>
+                            <input type="text" value={editedUsername} onChange={(e) => setEditedUsername(e.target.value)} />
+                            <button onClick={handleUsernameSave}>Save</button>
+                        </>
+                    ) : (
+                            <h2 style={{ paddingRight: "10px", marginLeft: "-40px" }}>Chatting as: {username}</h2>
+                        )}
+                    <input 
+                        style={{ width: "20px", height: "20px" }}
+                        type="image"
+                        alt="edit icon"
+                        src="/edit-icon.png"
+                        onClick={handleUsernameEdit} />
+                </div>
+                <img style={{ width: "50px", height: "50px", padding: "0px", borderRadius: "10px", border: "1px solid #ccc" }} src="/nova.jpg" alt="nova icon" />
+            </div>
+        </div>
+    );
+};
+
+export default AppHeader;
